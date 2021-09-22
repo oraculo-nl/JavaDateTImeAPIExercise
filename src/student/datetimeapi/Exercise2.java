9b0f65fe35f2d4ee03b085e0f2c9e020cf53a8a8
William Chau <hq0132410@windesheim.nl>
2021-09-22 10:11:09 +0200

    exercise2 WIP

--- src/student/datetimeapi/Exercise2.java
+++ src/student/datetimeapi/Exercise2.java
@@ -1,4 +1,16 @@
 package student.datetimeapi;
 
+import java.time.Month;
+import java.time.YearMonth;
+
 public class Exercise2 {
+    public static void main(String[] args) {
+        //given an arbitrary year, print out the names of the months and the lengths of the months.
+        var year = 2001;
+        for (Month month : Month.values()) {
+            YearMonth ym = YearMonth.of(year, month);
+            System.out.printf("%s: %d days%n", month, ym.lengthOfMonth());
+        }
+    }
 }
+
